dc606d3fba29f63ec2045acd1b79fc44791b8656
Mohammad Mahdi Saffari <mmsaffari@gmail.com>
2017-12-28 16:36:52 +0330

    Initial Commit

---
+++ Air Quality Index/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Air Quality Index
+//
+//  Created by Mohammad M. Saffari on 12/28/17.
+//  Copyright © 2017 DNE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
